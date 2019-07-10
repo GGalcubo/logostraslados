f319042aa8d038c5a8b8dd7555a34bdccd50d172
jeremias <jeremias.cuta@virtuespin.com>
2019-07-10 18:20:16 -0300

    first commit

---
+++ js/rollover.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+    $("#menu-concepto").mouseover(function(){
+        $("#boton-concepto").css('background-image', 'url(' + 'img/1.png' + ')');
+    });
+    $("#menu-concepto").mouseout(function(){
+        $("#boton-concepto").css('background-image', 'url()');
+    });
+    $("#boton-concepto").mouseover(function(){
+        $("#boton-concepto").css('background-image', 'url(' + 'img/1.png' + ')');
+    });
+    $("#boton-concepto").mouseout(function(){
+        $("#boton-concepto").css('background-image', 'url()');
+    });
+});
+
+$(document).ready(function(){
+    $("#menu-servicios").mouseover(function(){
+        $("#boton-servicios").css('background-image', 'url(' + 'img/2.png' + ')');
+    });
+    $("#menu-servicios").mouseout(function(){
+        $("#boton-servicios").css('background-image', 'url()');
+    }); 
+    $("#boton-servicios").mouseover(function(){
+        $("#boton-servicios").css('background-image', 'url(' + 'img/2.png' + ')');
+    });
+    $("#boton-servicios").mouseout(function(){
+        $("#boton-servicios").css('background-image', 'url()');
+    });
+});
+
+$(document).ready(function(){
+    $("#menu-flota").mouseover(function(){
+        $("#boton-flota").css('background-image', 'url(' + 'img/3.png' + ')');
+    });
+    $("#menu-flota").mouseout(function(){
+        $("#boton-flota").css('background-image', 'url()');
+    });
+    $("#boton-flota").mouseover(function(){
+        $("#boton-flota").css('background-image', 'url(' + 'img/3.png' + ')');
+    });
+    $("#boton-flota").mouseout(function(){
+        $("#boton-flota").css('background-image', 'url()');
+    });
+});
+
+$(document).ready(function(){
+    $("#menu-clientes").mouseover(function(){
+        $("#boton-clientes").css('background-image', 'url(' + 'img/4.png' + ')');
+    });
+    $("#menu-clientes").mouseout(function(){
+        $("#boton-clientes").css('background-image', 'url()');
+    });
+    $("#boton-clientes").mouseover(function(){
+        $("#boton-clientes").css('background-image', 'url(' + 'img/4.png' + ')');
+    });
+    $("#boton-clientes").mouseout(function(){
+        $("#boton-clientes").css('background-image', 'url()');
+    });
+});
+
+$(document).ready(function(){
+    $("#menu-misTraslados").mouseover(function(){
+        $("#boton-misTraslados").css('background-image', 'url(' + 'img/5.png' + ')');
+    });
+    $("#menu-misTraslados").mouseout(function(){
+        $("#boton-misTraslados").css('background-image', 'url()');
+    });
+    $("#boton-misTraslados").mouseover(function(){
+        $("#boton-misTraslados").css('background-image', 'url(' + 'img/5.png' + ')');
+    });
+    $("#boton-misTraslados").mouseout(function(){
+        $("#boton-misTraslados").css('background-image', 'url()');
+    });
+}); 
+
+$(document).ready(function(){
+    $("#menu-conciencia").mouseover(function(){
+        $("#boton-conciencia").css('background-image', 'url(' + 'img/6.png' + ')');
+    });
+    $("#menu-conciencia").mouseout(function(){
+        $("#boton-conciencia").css('background-image', 'url()');
+    });
+    $("#boton-conciencia").mouseover(function(){
+        $("#boton-conciencia").css('background-image', 'url(' + 'img/6.png' + ')');
+    });
+    $("#boton-conciencia").mouseout(function(){
+        $("#boton-conciencia").css('background-image', 'url()');
+    });
+});
+
+$(document).ready(function(){
+    $("#menu-contacto").mouseover(function(){
+        $("#boton-contacto").css('background-image', 'url(' + 'img/7.png' + ')');
+    });
+    $("#menu-contacto").mouseout(function(){
+        $("#boton-contacto").css('background-image', 'url()');
+    });
+    $("#boton-contacto").mouseover(function(){
+        $("#boton-contacto").css('background-image', 'url(' + 'img/7.png' + ')');
+    });
+    $("#boton-contacto").mouseout(function(){
+        $("#boton-contacto").css('background-image', 'url()');
+    });
+});
